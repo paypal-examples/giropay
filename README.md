9bfa979ef369bb1e9c26528d750f5d3a1839e4f8
andy <xxxandyfenxxx@xxx.com>
2021-01-22 14:17:46 -0800

    add heroku

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-See a [hosted version](https://andyfen.github.io/codesandbox/) of the sample or fork a copy on [codesandbox.io](https://githubbox.com/andyfen/codesandbox/tree/master)
+# Paying with Giropay
+
+This integration uses the JavaScript SDK to accept Giropay payments
 
 
+See a [hosted version](https://demo-giropay-js-sdk.herokuapp.com) of the sample
 
 
-[![Open in CodeSandbox](https://img.shields.io/badge/Open%20in-CodeSandbox-blue?style=flat-square&logo=codesandbox)](https://githubbox.com/andyfen/codesandbox/tree/master)
+### How to run locally
+
+Copy the .env.example file into a file named .env
+
+```
+cp .env.example .env
+```
+
+and configuring your .env config file with your Paypal ClientId and ClientSecret
+
+1. Clone the repo  `git clone git@github.com:paypal-examples/giropay-payment.git`
+2. Run `npm install`
+3. Run `npm run dev`
+4. Navigate to `http://localhost:8080/`
+
--- client/index.js
+++ client/index.js
@@ -1,9 +1,129 @@
+/* eslint-disable consistent-return, new-cap, no-alert, no-console */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>..
-</div>
-`;
+const order = {
+  purchase_units: [
+    {
+      amount: {
+        currency_code: 'EUR',
+        value: '49.11',
+      },
+    },
+  ],
+  application_context: {
+    return_url: `${window.location.origin}/success.html`,
+    cancel_url: `${window.location.origin}/cancel.html`,
+  },
+}
+
+/* Paypal */
+paypal
+  .Marks({
+    fundingSource: paypal.FUNDING.PAYPAL,
+  })
+  .render('#paypal-mark')
+
+paypal
+  .Buttons({
+    fundingSource: paypal.FUNDING.PAYPAL,
+    style: {
+      label: 'pay',
+    },
+    createOrder(data, actions) {
+      return actions.order.create(order)
+    },
+    onApprove(data, actions) {
+      return actions.order.capture().then(function(details) {
+        alert(`Transaction completed by ${details.payer.name.given_name}!`)
+      })
+    },
+  })
+  .render('#paypal-btn')
+
+/* Giropay */
+paypal
+  .Marks({
+    fundingSource: paypal.FUNDING.GIROPAY,
+  })
+  .render('#giropay-mark')
+
+paypal
+  .Fields({
+    fundingSource: paypal.FUNDING.GIROPAY,
+    style: {
+      base: {
+        backgroundColor: 'white',
+        color: 'black',
+        fontSize: '16px',
+        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+        lineHeight: '1.4',
+        letterSpacing: '0.3',
+      },
+      input: {
+        backgroundColor: 'white',
+        fontSize: '16px',
+        color: '#333',
+        borderColor: '#dbdbdb',
+        borderRadius: '4px',
+        borderWidth: '1px',
+        padding: '1rem',
+      },
+      invalid: {
+        color: 'red',
+      },
+      active: {
+        color: 'black',
+      },
+    },
+    fields: {
+      name: {
+        value: '',
+        hidden: false,
+      },
+    },
+  })
+  .render('#giropay-container')
+
+paypal
+  .Buttons({
+    fundingSource: paypal.FUNDING.GIROPAY,
+    style: {
+      label: 'pay',
+    },
+    createOrder(data, actions) {
+      return actions.order.create(order)
+    },
+    onApprove(data, actions) {
+      // capture is called after recieving a webhook event on the server
+    },
+  })
+  .render('#giropay-btn')
+
+/* radio buttons */
+
+// Listen for changes to the radio buttons
+document.querySelectorAll('input[name=payment-option]').forEach(el => {
+  // handle button toggles
+  el.addEventListener('change', event => {
+    switch (event.target.value) {
+      case 'paypal':
+        document.getElementById('giropay-container').style.display = 'none'
+        document.getElementById('giropay-btn').style.display = 'none'
+
+        document.getElementById('paypal-btn').style.display = 'block'
+
+        break
+      case 'giropay':
+        document.getElementById('giropay-container').style.display = 'block'
+        document.getElementById('giropay-btn').style.display = 'block'
+
+        document.getElementById('paypal-btn').style.display = 'none'
+        break
+
+      default:
+        break
+    }
+  })
+})
+
+document.getElementById('giropay-container').style.display = 'none'
+document.getElementById('giropay-btn').style.display = 'none'
